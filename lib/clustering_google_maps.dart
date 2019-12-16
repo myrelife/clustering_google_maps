53eac2c9365511eb0ad6f54793ffd633818af6bb
Kent Chien <kent@myre.life>
2019-12-16 10:39:37 +0800

    refactor: export aggregated_points

--- lib/clustering_google_maps.dart
+++ lib/clustering_google_maps.dart
@@ -1,3 +1,4 @@
 export 'package:clustering_google_maps/src/clustering_helper.dart';
 export 'package:clustering_google_maps/src/aggregation_setup.dart';
 export 'package:clustering_google_maps/src/lat_lang_geohash.dart';
+export 'package:clustering_google_maps/src/aggregated_points.dart';
